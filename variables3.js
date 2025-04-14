06edf0a1ee5f79bf7993ab1b28c68058efa4e485
Ignacio <ignacionazar9@gmail.com>
2025-04-14 15:08:49 -0300

    realize el tercer ejercicio

---
+++ variables3.js
@@ -0,0 +1,8 @@
+//ejemplo de suma de dos numeros
+let numero1, numero2, resultado;
+numero1 = 3;
+numero2 = 5;
+
+resultado = numero1 + numero2;
+
+document.writeln("El resultado de la suma es: " + resultado);
