6b7b93b4d984645ea7ab3b5b80f06bc83e0d88c0
Ignacio <ignacionazar9@gmail.com>
2025-04-16 15:00:14 -0300

    agregue el cuarto ejercicio de PSeint

--- ejercicio4PSeint.js
+++ ejercicio4PSeint.js
@@ -1,1 +1,6 @@
-const numero1 = parseInt(prompt("Ingrese un numero")),
+const numero1 = parseInt(prompt("Ingrese un numero"));
+if (numero1 % 2 === 0) {
+  document.writeln("El numero es par");
+} else {
+  document.writeln("El numero es impar");
+}
