fd708495d50f54168f8af626209faca92c36e358
Ignacio <ignacionazar9@gmail.com>
2025-04-16 15:41:27 -0300

    agregue el septimo ejercicio de PSeint

---
+++ ejercicio6PSeint.js
@@ -0,0 +1,3 @@
+let temperatura = prompt("Ingrese la temperatura en grados Fahrenheit (°F): ");
+let resultado = (5 / 9) * (temperatura - 32);
+document.writeln("La temperatura en grados Celsius (°C) es: ", resultado);
---
+++ ejercicio7PSeint.js
@@ -0,0 +1,6 @@
+let largo = parseFloat(prompt("Ingrese el largo de un Rectangulo: "));
+let ancho = parseFloat(prompt("Ingrese el ancho de un Rectangulo: "));
+let perímetro = 2 * (largo + ancho);
+document.writeln("El perimetro del rectangulo es: ", perímetro);
+let área = largo * ancho;
+document.writeln("El área del rectangulo es: ", área);
