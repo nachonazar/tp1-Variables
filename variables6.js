f81cbc59d9d2a3ac1974d69c5899646e951067a3
Ignacio <ignacionazar9@gmail.com>
2025-04-16 00:42:25 -0300

    hice una modificacion en el ejercicio seis

--- variables6.js
+++ variables6.js
@@ -2,8 +2,8 @@ const numero1 = parseInt(prompt("Ingrese un numero")),
   numero2 = parseInt(prompt("Ingrese un segundo numero"));
 
 if (numero1 > numero2) {
-  console.log("El mayor numero es: ", numero1);
+  document.writeln("El mayor numero es: ", numero1);
 }
 else{
-    console.log("El mayor numero es: ", numero2);
+    document.writeln("El mayor numero es: ", numero2);
 }
